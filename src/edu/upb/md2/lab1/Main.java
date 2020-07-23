69cadda2d4b5113ceeb6f82b0635aedabd4a4d3c
Pedro Felipe Gomez Bonilla <pedro.gomez.2019@upb.edu.co>
2020-07-23 07:18:44 -0500

    Casos creados

--- src/edu/upb/md2/lab1/Main.java
+++ src/edu/upb/md2/lab1/Main.java
@@ -1,8 +1,15 @@
 package edu.upb.md2.lab1;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner sc = new Scanner(System.in);
+        int casos = sc.nextInt();
+        for(int i=0;i<casos;i++){
+            System.out.printf("test");
+        }
     }
 }
